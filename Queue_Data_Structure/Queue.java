1c60150adae0a5bb985fb0459362349a94b9ac7b
Adityapithva <121885084+Adityapithva@users.noreply.github.com>
2024-03-29 11:16:53 +0530

    add element

---
+++ Queue_Data_Structure/Queue.java
@@ -0,0 +1,53 @@
+package Queue_Data_Structure;
+public class Queue {
+    private ListNode front;
+    private ListNode rear;
+    private int length;
+    private class ListNode{
+        private int data;
+        private ListNode next;
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length == 0;
+    }
+    public Queue(){
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
+    }
+    public void print(){
+        if(isEmpty()){
+            return;
+        }
+        ListNode current = front;
+        while(current != null){
+            System.out.print(current.data +" --> ");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+    public void enqueue(int data){
+        ListNode temp = new ListNode(data);
+        if(isEmpty()){
+            front = temp;
+        }else{
+            rear.next = temp;
+        }
+        rear = temp;
+        length++;
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        q.print();
+    }
+}
